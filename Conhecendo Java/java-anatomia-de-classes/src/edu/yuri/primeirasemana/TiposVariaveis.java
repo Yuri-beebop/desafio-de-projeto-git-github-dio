d0f190fc20d0d4e5ec3d0a04944126eb7f41bb6a
Yuri-beebop <yuricristiansantos1001@gmail.com>
2022-12-29 19:07:42 -0300

    exemplo Tipo e Variaveis

---
+++ Conhecendo Java/java-anatomia-de-classes/src/edu/yuri/primeirasemana/TiposVariaveis.java
@@ -0,0 +1,16 @@
+package edu.yuri.primeirasemana;
+
+public class TiposVariaveis {
+
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2500;
+        
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        final int numero = 1;  //final não permite que o valor da variavel seja alterada
+    
+    }
+    
+}
