cb3810f5ce4ed559181b97dc504da1b8ec455dbd
Yuri-beebop <yuricristiansantos1001@gmail.com>
2023-01-02 16:37:34 -0300

    Exercicios do curso java

--- Conhecendo Java/java-anatomia-de-classes/src/edu/yuri/primeirasemana/TiposVariaveis.java
+++ Conhecendo Java/java-anatomia-de-classes/src/edu/yuri/primeirasemana/TiposVariaveis.java
@@ -1,6 +1,6 @@
 package edu.yuri.primeirasemana;
 
-public class TiposVariaveis {
+public class TiposVariaveis{
 
     public static void main(String[] args) throws Exception {
         double salarioMinimo = 2500;
@@ -9,7 +9,7 @@ public class TiposVariaveis {
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal;
 
-        final int numero = 1;  //final não permite que o valor da variavel seja alterada
+        final int numero = 1;
     
     }
 
---
+++ Conhecendo Java/java-anatomia-de-classes/src/edu/yuri/segundasemana/TerminalArgumentos/MinhaClass.java
@@ -0,0 +1,10 @@
+package edu.yuri.segundasemana.TerminalArgumentos;
+
+public class MinhaClass{
+
+    public static void main(String[] args) {
+
+        System.out.println("Ola, fui executado no termnal :)");
+
+    }
+}
